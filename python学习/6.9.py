88ed031acb3bf2c28ddc841b32c096d7efbc360e
史济通 <25800190119@m.fudan.edu.cn>
2026-09-13 16:39:23 +0800

    更改

---
+++ python学习/6.9.py
@@ -0,0 +1,6 @@
+favorite_places = {'Alice': ['Beijing', 'Hangzhou'],
+                   'Bob': ['Chengdu', 'Chongqing', "Xi'an"],
+                   'Cindy': ['Suzhou']}
+for person, places in favorite_places.items():
+    print(person)
+    print(places)
