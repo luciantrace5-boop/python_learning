546e8288b1ae1994f37775d050d519ba5f19e908
史济通 <25800190119@m.fudan.edu.cn>
2026-09-13 16:27:51 +0800

    更改

---
+++ python学习/6.5.py
@@ -0,0 +1,9 @@
+river_country_dict = {
+    'Nile River':'Egypt',
+    'Amazon River':'Brazil',
+    'Yangtze River':'China',
+}
+for river, country in river_country_dict.items():
+    print(f'The {river} runs through {country}.')
+    print(river)
+    print(country)
---
+++ python学习/6.7.py
@@ -0,0 +1,7 @@
+person = {'first_name': 'Trace', 'last_name': 'Lucian', 'age': 20, 'city': 'shanghai'}
+man = {'first_name': 'Tony', 'last_name': 'Brown'}
+woman = {'first_name': 'Sarah', 'last_name': 'Albert'}
+people = [person, man, woman]
+for person in people:
+    print(person)
+# :后加空格 ，后也加空格
